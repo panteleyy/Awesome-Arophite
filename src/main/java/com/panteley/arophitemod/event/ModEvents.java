2b526d25ff7fb0555971a266275f83ab93cce219
panteleyy <212159011+panteleyy@users.noreply.github.com>
2026-05-12 20:05:55 +0500

    Add function that returns effect based on quantity instability

--- src/main/java/com/panteley/arophitemod/event/ModEvents.java
+++ src/main/java/com/panteley/arophitemod/event/ModEvents.java
@@ -4,12 +4,13 @@ import com.panteley.arophitemod.attachment.ModAttachments;
 import com.panteley.arophitemod.block.ModBlocks;
 import com.panteley.arophitemod.item.ModItems;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
-import net.minecraft.world.level.block.Blocks;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 
 @EventBusSubscriber
 public class ModEvents {
@@ -35,7 +36,12 @@ public class ModEvents {
     @SubscribeEvent
     public static void onCraftItem(PlayerEvent.ItemCraftedEvent event) {
         if (event.getEntity() instanceof ServerPlayer player) {
-            if (event.getCrafting().getItem() == ModItems.UNSTABLE_AROPHITE_PICKAXE.get()) {
+            if (event.getCrafting().getItem() == ModItems.UNSTABLE_AROPHITE_PICKAXE.get() ||
+                    event.getCrafting().getItem() == ModItems.UNSTABLE_AROPHITE_AXE.get() ||
+                    event.getCrafting().getItem() == ModItems.UNSTABLE_AROPHITE_SWORD.get() ||
+                    event.getCrafting().getItem() == ModItems.UNSTABLE_AROPHITE_SHOVEL.get() ||
+                    event.getCrafting().getItem() == ModItems.UNSTABLE_AROPHITE_HOE.get()) {
+
 
                 System.out.println("Игрок жестко СКРАФТИЛ из арофитикс");
                 int current_instability = player.getData(ModAttachments.EFFECT_INSTABILITY);
@@ -44,6 +50,25 @@ public class ModEvents {
                 System.out.println("Его нестабильность " + player.getData(ModAttachments.EFFECT_INSTABILITY));
             }
         }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerTick(PlayerTickEvent.Post event) {
+
+        if (!(event.getEntity() instanceof ServerPlayer player)) {
+                return;
+        }
+
+        int current_instability = player.getData(ModAttachments.EFFECT_INSTABILITY);
+        if (player.tickCount % 300 == 0) {
+
+            if (current_instability > 100) {
+                player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED,
+                        200, 1, false, true, true));
+            }
+        }
+
+
     }
 }
 
