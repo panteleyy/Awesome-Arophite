1e9d7c4633e95340b5b4ef65b917f06b548be88d
panteleyy <212159011+panteleyy@users.noreply.github.com>
2026-05-15 16:55:27 +0500

    Add visual effects to lightning charging mechanic

--- src/main/java/com/panteley/arophitemod/block/custom/ArophiteCharger.java
+++ src/main/java/com/panteley/arophitemod/block/custom/ArophiteCharger.java
@@ -2,6 +2,10 @@ package com.panteley.arophitemod.block.custom;
 
 import com.panteley.arophitemod.item.ModItems;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleType;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LightningBolt;
@@ -21,13 +25,19 @@ public class ArophiteCharger extends Block {
     public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
         if (entity instanceof ItemEntity itemEntity) {
             if (itemEntity.getItem().getItem() == ModItems.UNSTABLE_AROPHITE_DUST.get()) {
-                LightningBolt lightning = new LightningBolt(EntityType.LIGHTNING_BOLT, level);
-                lightning.moveTo(pos.getX(), pos.getY(), pos.getZ());
-                lightning.setVisualOnly(true);
-                level.addFreshEntity(lightning);
 
-                itemEntity.setItem(new ItemStack(ModItems.CHARGED_AROPHITE_DUST.get(), itemEntity.getItem().getCount()));
+                if (level instanceof ServerLevel serverLevel) {
+                    LightningBolt lightning = new LightningBolt(EntityType.LIGHTNING_BOLT, level);
+                    lightning.moveTo(pos.getX(), pos.getY(), pos.getZ());
+                    lightning.setVisualOnly(true);
+                    level.addFreshEntity(lightning);
 
+                    serverLevel.sendParticles(ParticleTypes.ELECTRIC_SPARK,
+                            pos.getX(), pos.getY(), pos.getZ(),
+                            200, 2, 2, 2, 0.5);
+
+                    itemEntity.setItem(new ItemStack(ModItems.CHARGED_AROPHITE_DUST.get(), itemEntity.getItem().getCount()));
+                }
             }
 
         }
